24e659b08222e96d17eb0409152993bc717a463a
Jake Lishman <jakelishman@gmail.com>
2017-09-29 17:08:47 +0100

    Simplify analytic.py
    This massively simplifies the logic used in analytic.py by grouping all of the
    calculations for different colours into one place.  Most of the calculations are
    the same regardless of colour.  This also changes the parameters in the
    calculations from ground/excited to into/out_of, since most of the logic and
    swapping between them is based on this, not on which one is the ground.
    
    This setup should also make it easier to implement a tree to find all of the
    pulses, since the algorithm's choices are much easier to change---for the
    carrier now we only have to change the `target` parameter to 'e' rather than 'g'
    and the logic will propagate it through correctly.

--- analytic.py
+++ analytic.py
@@ -3,287 +3,197 @@ from math import *
 from numpy import angle as arg
 from operator import xor
 import numpy as np
-import state_specifier as state
+import state_specifier as ss
 
 def bound_angle(angle):
     """Get the angle (divided by pi) bounded on (-1, 1]."""
     out = fmod(angle, 2.0)
     return out if -1.0 < out <= 1.0 else out - np.sign(out) * 2.0
 
-def _feq(a, b):
+def feq(a, b):
     """Compare two floats for near equality."""
     return abs(a - b) < 1e-8
 
-def _validate_target(internal, motional):
-    """Check the target of an angle calculator is valid."""
-    if not (internal is 'g' or internal is 'e'):
-        raise TypeError("The internal state must be 'g' or 'e', but I got: {}"\
-                        .format(internal))
-    if not (isinstance(motional, int) and motional >= 0):
-        raise TypeError(
-            "The motional state must be an unsigned integer, but I got: {}"\
-            .format(motional))
-    return
+def other_coupled_element(colour, element):
+    """other_coupled_element(colour, element) -> other_element
 
-def _find_angle(a, b, neg):
-    """_find_angle(a, b, neg) -> angle
-
-    Find the smallest positive angle `x` which satisfies
-        x = 2 arctan( (-1 if neg else 1) * (|a| +- sqrt(|a|^2 + |b|^2)) / |b| ).
-    This is the base equation which needs a solution for all of the sideband
-    pulses.
+    Given one of the states coupled by a colour operator, get the state which is
+    coupled to it.
 
     Arguments:
-    a: complex float -- One of the complex coefficients of a state pair.
-    b: complex float -- One of the complex coefficients of a state pair.
-    neg: Boolean -- Whether the arctan argument should be negated.
+    - colour: 'c' | 'r' | 'b' -- The colour of the pulse applied.
+    - element: state_specifier -- One element of the coupled pair.
 
     Returns:
-    angle: real float -- The smallest positive solution for `x`, divided by pi.
-    """
-    if _feq(abs(b), 0.0):
-        return 1.0
-    root = sqrt(abs(a) ** 2 + abs(b) ** 2)
-    return (2.0 / pi) * atan(((-1.0 if neg else 1.0) * abs(a) + root) / abs(b))
+    - other_element: state_specifier -- The other coupled element.
 
-def carrier_pulse(internal, motional, excited, ground, adjoint = False):
-    """carrier_pulse(internal, motional, excited, ground, adjoint)
-    -> (angle, coeff)
+    Throws:
+    - ValueError: If the given `element` does not interact with the pulse."""
+    _int, _mot = ss.internal(element), ss.motional(element)
+    if _int is {'c': None, 'r': 'g', 'b': 'e'}[colour] and _mot is 0:
+        raise ValueError("The {} pulse doesn't interact with |{}0>."\
+            .format(*{'r': ("red", 'g'), 'b': ("blue", 'e')}[colour]))
+    _o_int = 'g' if _int is 'e' else 'e'
+    if colour is 'c':
+        return (_mot, _o_int)
+    elif (colour is 'r' and _int is 'g') or (colour is 'b' and _int is 'e'):
+        return (_mot - 1, _o_int)
+    else:
+        return (_mot + 1, _o_int)
 
-    Choose an angle for a carrier pulse such that the populations in the ground
-    and excited states will be moved completely into one or the other.  In other
-    words: Find an `x` and an `a` such that
-        c(x) . (ex |en> + gr |gn>) = e^(i a) sqrt(|ex|^2 + |gr|^2) |?n>,
-    where `?` is `internal`, and `n` is `motional`.  There will only be
-    solutions if `excited` and `ground` are pi/2 out of phase with each other
-    (e.g. one is real, the other purely imaginary).
+def phase_neg(colour, into, out_of, into_is_ground):
+    """phase_neg(colour, into, out_of, into_is_ground) -> neg
+
+    Return a Boolean saying whether there is a relative negative sign due to the
+    phase difference between the two states.
 
     Arguments:
-    internal: 'g' | 'e' --
-        Whether we should end up in the ground or excited state at the end.
-
-    motional: unsigned int --
-        The motional level we should end up in.  The populations referred to be
-        `excited` and `ground` should both be in this level, since they're the
-        ones the carrier effects.  This parameter is ignored, since it's not
-        relevant to the carrier.
-
-    excited: complex float --
-        The starting complex coefficient of the |en> state.  Must be pi/2 out of
-        phase with `ground`.
-
-    ground: complex float --
-        The starting complex coefficient of the |gn> state.  Must be pi/2 out of
-        phase with `excited`.
-
-    adjoint (Optional, False): Boolean --
-        Whether we're finding an adjoint pulse or a regular pulse.
+    - colour: 'c' | 'r' | 'b' -- The colour of the pulse applied.
+    - into: complex float --
+        The coefficient of the element the pulse is moving the population into.
+    - out_of: complex float --
+        The coefficient of the element the pulse is moving the population out
+        of.
+    - into_is_ground: boolean --
+        True if the pulse is moving the population into the ground state, and
+        False if the pulse is moving the population into the excited state.
 
     Returns:
-    angle: real float --
-        The angle of the carrier pulse, divided by pi.
+    - neg: boolean --
+        True if there is a negation associated with the phase difference of the
+        two coefficients (e.g. sin(delta) = -1 for the carrier).
 
-    coeff: complex float --
-        The ending complex coefficient of the populated state.
-    """
-    _validate_target(internal, motional)
-    delta = bound_angle((arg(ground) - arg(excited)) / pi)
-    needs = [
-        _feq(abs(excited) ** 2, 0.0) and internal is 'e',
-        _feq(abs(ground) ** 2, 0.0) and internal is 'g',
-        _feq(abs(delta), 0.5),
-    ]
-    if not any(needs):
+    Throws:
+    - ValueError --
+        If complete population movement cannot be achieved due to the phase
+        difference.
+
+    Notes:
+        If one of the coefficients is 0, then this function always returns
+        False, even though both True and False are valid return values."""
+    if feq(abs(into), 0.0) or feq(abs(out_of), 0.0):
+        return False
+    delta = bound_angle((arg(into) - arg(out_of)) / pi)
+    allowed_deltas = [-0.5, 0.5] if colour is 'c' else [-1.0, 0.0, 1.0]
+    if not any(map(lambda x: feq(delta, x), allowed_deltas)):
         raise ValueError(
-            "The phase difference between coefficients in a carrier pulse "
-            "should be +-pi/2, but I have '{}' and '{}', giving '{}'."\
-            .format(excited, ground, delta))
-    neg = reduce(xor, [internal is 'e', _feq(delta, -0.5), adjoint], False)
-    if internal is 'e':
-        ground, excited = excited, ground
-    angle = _find_angle(ground, excited, neg)
-    coeff = ground * cos(pi * angle)\
-            - 1.0j * excited * sin(pi * angle) * (-1.0 if adjoint else 1.0)
-    return angle, coeff
+            "The {} pulse needs a phase difference as one of {}, "
+            "but I got {} and {} which is a difference of {}."\
+                .format({'c': 'carrier', 'r': 'red', 'b': 'blue'}[colour],
+                        allowed_deltas,
+                        arg(into) / pi, arg(out_of) / pi, delta))
+    if colour is 'c':
+        return feq(delta, -0.5)
+    else:
+        return into_is_ground != feq(delta, 0.0)
 
-def red_pulse(internal, motional, excited, ground, adjoint = False):
-    """red_pulse(internal, motional, excited, ground, adjoint)
-    -> (angle, coeff)
+def single_pulse(colour, target, state_vector, adjoint = True):
+    """single_pulse(colour, target, state_vector, adjoint = True)
+    -> angle
 
-    Choose an angle for a red pulse such that the populations in the ground
-    and excited states will be moved completely into one or the other.  In other
-    words: Find an `x` and an `a` such that
-        r(x) . (ex |e> + gr |g>) = e^(i a) sqrt(|ex|^2 + |gr|^2) |?n>,
-    where `?` is `internal`, and `n` is `motional`.  There will only be
-    solutions if `excited` and `ground` are in phase, or pi out of phase with
-    each other (e.g. one is real positive, the other real negative).
-
-    Arguments:
-    internal: 'g' | 'e' --
-        Whether we should end up in the ground or excited state at the end.
-
-    motional: unsigned int --
-        The motional level we should end up in.  If `internal` is 'g', then the
-        coupled states will be |en-1> and |gn>, or if it's 'e', they'll be |en>
-        and |gn+1>.
-
-    excited: complex float --
-        The starting complex coefficient of the |e> state.  Must be either in
-        phase with, or pi out of phase with `ground`.
-
-    ground: complex float --
-        The starting complex coefficient of the |e> state.  Must be either in
-        phase with, or pi out of phase with `excited`.
-
-    adjoint (Optional, False): Boolean --
-        Whether we're finding an adjoint pulse or a regular pulse.
-
-    Returns:
-    angle: real float --
-        The angle of the pulse, divided by pi.
-
-    coeff: complex float --
-        The ending complex coefficient of the populated state.
-    """
-    _validate_target(internal, motional)
-    if motional == 0 and internal is 'g':
-        raise ValueError("The red pulse doesn't interact with |g0>.")
-    delta = bound_angle((arg(ground) - arg(excited)) / pi)
-    needs = [
-        _feq(abs(excited) ** 2, 0.0) and internal is 'e',
-        _feq(abs(ground) ** 2, 0.0) and internal is 'g',
-        _feq(abs(delta), 0.0) or _feq(abs(delta), 1.0),
-    ]
-    if not any(needs):
-        raise ValueError(
-            "The phase difference between coefficients in a red pulse "
-            "should be +-1 or 0, but I have '{}' and '{}', giving '{}'."\
-            .format(excited, ground, delta))
-    neg = reduce(xor, [internal is 'e', _feq(abs(delta), 1.0), adjoint], False)
-    if internal is 'g':
-        root = sqrt(motional)
-        angle = _find_angle(ground, excited, neg) / root
-        coeff = ground * cos(root * angle * pi)\
-                + excited * sin(root * angle * pi) * (-1.0 if adjoint else 1.0)
-    else: # internal is 'e'
-        root = sqrt(motional + 1)
-        angle = _find_angle(excited, ground, neg) / root
-        coeff = excited * cos(root * angle * pi)\
-                - ground * sin(root * angle * pi) * (-1.0 if adjoint else 1.0)
-    return angle, coeff
-
-def blue_pulse(internal, motional, excited, ground, adjoint = False):
-    """blue_pulse(internal, motional, excited, ground, adjoint)
-    -> (angle, coeff)
-
-    Choose an angle for a red pulse such that the populations in the ground
-    and excited states will be moved completely into one or the other.  In other
-    words: Find an `x` and an `a` such that
-        b(x) . (ex |e> + gr |g>) = e^(i a) sqrt(|ex|^2 + |gr|^2) |?n>,
-    where `?` is `internal`, and `n` is `motional`.  There will only be
-    solutions if `excited` and `ground` are in phase, or pi out of phase with
-    each other (e.g. one is real positive, the other real negative).
+    Choose an angle for a pulse of `colour` which moves all the population of
+    the state coupled to `target` into `target`.  In other words, find a `phi`
+    such that
+        colour(phi) . (a |target> + b |coupled to target>)
+            = e^(i theta) sqrt(|a|^2 + |b|^2) |target>
+    for some `theta`.  `a` and `b` are the coefficients of the two states, which
+    the function will read from `state_vector`.  These must be of the correct
+    phase difference.
 
     Arguments:
-    internal: 'g' | 'e' --
-        Whether we should end up in the ground or excited state at the end.
-
-    motional: unsigned int --
-        The motional level we should end up in.  If `internal` is 'g', then the
-        coupled states will be |en+1> and |gn>, or if it's 'e', they'll be |en>
-        and |gn-1>.
-
-    excited: complex float --
-        The starting complex coefficient of the |e> state.  Must be either in
-        phase with, or pi out of phase with `ground`.
-
-    ground: complex float --
-        The starting complex coefficient of the |e> state.  Must be either in
-        phase with, or pi out of phase with `excited`.
-
-    adjoint (Optional, False): Boolean --
-        Whether we're finding an adjoint pulse or a regular pulse.
+    - colour: 'c' | 'r' | 'b' -- The colour of the pulse applied.
+    - target: state_specifier -- The element to move the population into.
+    - state_vector: 1D numpy.array of complex float --
+        The vector form of the ket of the current state of the system.
+    - adjoint (optional, True): boolean --
+        Whether the pulse is an adjoint pulse or not.  For finding the sequence
+        of pulses, this will usually be True.
 
     Returns:
-    angle: real float --
-        The angle of the pulse, divided by pi.
+    - angle: real float --
+        The angle `phi` from the summary, divided by pi.
 
-    coeff: complex float --
-        The ending complex coefficient of the populated state.
-    """
-    _validate_target(internal, motional)
-    if motional == 0 and internal is 'e':
-        raise ValueError("The blue pulse doesn't interact with |e0>.")
-    delta = bound_angle((arg(ground) - arg(excited)) / pi)
-    needs = [
-        _feq(abs(excited) ** 2, 0.0) and internal is 'e',
-        _feq(abs(ground) ** 2, 0.0) and internal is 'g',
-        _feq(abs(delta), 0.0) or _feq(abs(delta), 1.0),
-    ]
-    if not any(needs):
-        raise ValueError(
-            "The phase difference between coefficients in a red pulse "
-            "should be +-1 or 0, but I have '{}' and '{}', giving '{}'."\
-            .format(excited, ground, delta))
-    neg = reduce(xor, [internal is 'e', _feq(abs(delta), 1.0), adjoint], False)
-    if internal is 'g':
-        root = sqrt(motional + 1)
-        angle = _find_angle(ground, excited, neg) / root
-        coeff = ground * cos(root * angle * pi)\
-                + excited * sin(root * angle * pi) * (-1.0 if adjoint else 1.0)
-    else: # internal is 'e'
-        root = sqrt(motional)
-        angle = _find_angle(excited, ground, neg) / root
-        coeff = excited * cos(root * angle * pi)\
-                - ground * sin(root * angle * pi) * (-1.0 if adjoint else 1.0)
-    return angle, coeff
-
-def is_populated(internal, motional, state_vector):
+    Throws:
+    - ValueError --
+        - If the `target` element isn't coupled by the colour specified.
+        - If the coefficients of the two coupled elements don't allow complete
+          population transfer."""
     ns = state_vector.shape[0] // 2
-    coeff = state_vector[state.idx((motional, internal), ns)]
-    return not _feq(abs(coeff) ** 2, 0.0)
+    other = other_coupled_element(colour, target)
+    into = state_vector[ss.idx(target, ns)]
+    out_of = state_vector[ss.idx(other, ns)]
+    k = 1.0 if colour is 'c'\
+        else 1.0 / sqrt(max(map(ss.motional, (target, other))))
+    if feq(abs(out_of), 0.0):
+        return k
+    neg = adjoint != phase_neg(colour, into, out_of, ss.internal(target) is 'g')
+    neg = -1.0 if neg else 1.0
+    pop = sqrt(abs(into) ** 2 + abs(out_of) ** 2)
+    return 2.0 * k * atan((neg * abs(into) + pop) / abs(out_of)) / pi
+
+def is_populated(element, state_vector):
+    """is_populated(element, state_vector) -> bool
+
+    Determine if the `element` (`state_specifier`) has non-zero probability."""
+    ns = state_vector.shape[0] // 2
+    return not feq(abs(state_vector[ss.idx(element, ns)]) ** 2, 0.0)
 
 def both_populated(motional, state_vector):
-    return is_populated('g', motional, state_vector)\
-           and is_populated('e', motional, state_vector)
+    """both_populated(motional, state_vector) -> bool
 
-def _try_fixed(target_spec):
-    maxn = max(map(state.motional, target_spec))
-    ns = maxn + 1
-    cur = build_state_vector(target_spec, ns)
-    carrier, red, blue = tuple(map(lambda x: ColourOperator(x, ns), "crb"))
-    pulses = []
-    counter = 2 * maxn + 1
-    while counter > 0:
-        if maxn == 0 and not is_populated('e', 0, cur):
-            return pulses
-        if maxn == 0 or both_populated(maxn, cur):
-            gr = cur[state.idx((maxn, 'g'), ns)]
-            ex = cur[state.idx((maxn, 'e'), ns)]
-            op = carrier
-            angle, _ = carrier_pulse('g', maxn, ex, gr, adjoint = True)
-        elif is_populated('e', maxn, cur):
-            gr = cur[state.idx((maxn - 1, 'g'), ns)]
-            ex = cur[state.idx((maxn, 'e'), ns)]
-            op = blue
-            angle, _ = blue_pulse('g', maxn - 1, ex, gr, adjoint = True)
-            maxn -= 1
-        elif is_populated('g', maxn, cur):
-            gr = cur[state.idx((maxn, 'g'), ns)]
-            ex = cur[state.idx((maxn - 1, 'e'), ns)]
-            op = red
-            angle, _ = red_pulse('e', maxn - 1, ex, gr, adjoint = True)
-            maxn -= 1
-        else:
-            assert False, "wtf."
-        pulses.append((op.colour, angle))
-        cur = np.dot(op.U(-angle), cur)
-        counter -= 1
-    assert False, "Failed to find the sequence."
+    Determine if both states of phonon number `motional` have non-zero
+    probability of being occupied."""
+    return is_populated((motional, 'g'), state_vector)\
+           and is_populated((motional, 'e'), state_vector)
 
-def _try(target_spec):
-    for i, t in enumerate(target_spec):
-        p = 0.0 if state.motional(t) % 2 == {'g':0, 'e':1}[state.internal(t)]\
+def chequerboard_phases(target_spec):
+    """chequerboard_phases(target_spec) -> new_target_spec
+
+    Apply the phase chequerboard to the targets specified so that a pulse
+    sequence to evolve to it will exist."""
+    def set_phase(t):
+        p = 0.0 if ss.motional(t) % 2 == {'g':0, 'e':1}[ss.internal(t)]\
             else 0.5
-        target_spec[i] = state.set_phase(t, p)
-    return _try_fixed(target_spec)
+        return ss.set_phase(t, p)
+    return map(set_phase, target_spec)
+
+def find_pulses(target_spec):
+    """find_pulses(target_spec) -> pulses
+
+    Find a sequence of pulses which evolve the state |g0> into the state
+    specified by `target_spec`.
+
+    Arguments:
+    - target_spec: array_like of state_specifier --
+        The states which should be equally populated after the pulse sequence.
+
+    Returns:
+    - pulses: list of (colour * angle) --
+        - colour: 'c' | 'r' | 'b' -- The colour of the pulse to apply.
+        - angle: real float -- The angle to apply the pulse for, divided by pi.
+
+    Notes:
+        This can technically throw `ValueError` and `AssertionError`, but both
+        of these imply logic errors in the underlying code and cannot be
+        recovered from."""
+    target_spec = chequerboard_phases(target_spec)
+    max_n = max(map(ss.motional, target_spec))
+    current_state = build_state_vector(target_spec, max_n + 1)
+    carrier, red, blue = tuple(map(lambda x: ColourOperator(x, max_n+1), "crb"))
+    pulses = []
+    for _ in xrange(2 * max_n + 2, 0, -1):
+        if max_n == 0 and not is_populated((0, 'e'), current_state):
+            return pulses
+        elif max_n == 0:
+            op, target = carrier, (0, 'g')
+        elif both_populated(max_n, current_state):
+            op, target = carrier, (max_n, 'g')
+        elif is_populated((max_n, 'e'), current_state):
+            op, target = blue, (max_n - 1, 'g')
+        else: # is_populated((max_n, 'g'), current_state)
+            op, target = red, (max_n - 1, 'e')
+        angle = single_pulse(op.colour, target, current_state, adjoint = True)
+        pulses.append((op.colour, angle))
+        current_state = np.dot(op.U(-angle), current_state)
+        max_n = max_n if op is carrier else max_n - 1
+    assert False, "Logic error: failed to find the sequence."
